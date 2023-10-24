d2e660979a82458f9e591708224f6521ee7d8c1e
Rauf Mazzidan <raufmazzidan@gmail.com>
2023-10-24 21:11:04 +0700

    feat: update finished when change level

--- src/containers/QuestionManagement/element/ListPacks.js
+++ src/containers/QuestionManagement/element/ListPacks.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DragDropContext, Droppable, Draggable, resetServerContext } from 'react-beautiful-dnd';
 import { IconGridDots } from '@tabler/icons-react';
 import { Box, Flex, Text, Title } from '@mantine/core';
-import { doc, updateDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { db } from '@/config/firebase';
 import { useRouter } from 'next/router';
 import { questionType } from '../utils/constant';
+import { getUserData } from '@/utils/common';
 
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
@@ -17,9 +18,35 @@ const reorder = (list, startIndex, endIndex) => {
 
 const ListPacks = ({ data, setData }) => {
   const router = useRouter();
+  const user = getUserData();
+
+  const [mentee, setMentee] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    setLoading(true);
+    const ref = query(collection(db, 'user'), where('mentor', '==', user.uid));
+    try {
+      const response = await getDocs(ref);
+      const res = response.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setMentee(res);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.uid) {
+      getData();
+    }
+  }, [user.uid]);
+
   const onSort = async (datas) => {
     const isOnlyOneActive = datas.filter(({ active }) => !!active).length < 2;
     try {
+      let firstQuestionId = '';
       await datas.forEach(async (data, index) => {
         const ref = doc(db, 'question', data.id);
         let payload = {
@@ -28,6 +55,7 @@ const ListPacks = ({ data, setData }) => {
 
         if (isOnlyOneActive) {
           if (index === 0) {
+            firstQuestionId = data.id;
             payload = {
               ...payload,
               active: true,
@@ -41,6 +69,17 @@ const ListPacks = ({ data, setData }) => {
         }
 
         await updateDoc(ref, payload);
+        await mentee.forEach(async ({ id, ...d }) => {
+          await fetch(`/api/user/${id}`, {
+            headers: { Authorization: user.accessToken },
+            method: 'PUT',
+            body: JSON.stringify({
+              ...d,
+              finished: d?.finished?.length === 1 ? [firstQuestionId] : d.finished,
+            }),
+            contentType: 'application/json',
+          });
+        });
       });
     } catch (error) {
       console.log('sort failure');
